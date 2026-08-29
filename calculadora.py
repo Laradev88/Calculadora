4d41bc5e0ffd404f15d0ce122006b84f9737c11d
Laradev88 <laradevincenti@hotmail.com>
2026-08-29 15:23:45 -0300

    agrego operacion division

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+print ("calculadora")
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
